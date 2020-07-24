94cdab3a041cd7422f4a079d58fe8b5949a038b5
Gia <33999927+QuantumPhysx2@users.noreply.github.com>
2020-07-24 22:16:15 +0930

    Update 1.0.0 - API and Test Template
    - Added API key
    - Added basic CSS template /w background
    - Added const variable which fetches current date data via API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const owmAPI = {
+    key: "5cceb36da4f7530937ddc6a1963aef8f",
+    base: "https://api.openweather.map.org/data/2.5"
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        
+        let day = days[d.getDay()];         // Read from 0 and 6
+        let date = d.getDate();             // Read from 0 and 30
+        let month = months[d.getMonth()];   // Read from 0 and 11
+        let year = d.getFullYear();         // Get full year
+        // Return the day, date, month and year using string manipulation -> ${var}
+        return `${day} ${date} ${month} ${year}`;
+    }
+
+    return (
+        <div className="app">
+            <main>
+                <div className="search-box">
+                    <input type="text" className="search-bar" placeholder="Search..." />
+                </div>
+                <div className="location-box">
+                    <div className="location">
+                        New Your City, US
+                    </div>
+                    <div className="date">
+                        {dateBuilder(new Date())}
+                    </div>
+                    <div className="weather-box">
+                        <div className="temp">
+                            15°c
+                        </div>
+                        <div className="weather">
+                            Sunny
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
